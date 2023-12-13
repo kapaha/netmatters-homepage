775cae02ddd101a3ebfa2e0ff32d5a40800627ac
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2023-12-13 14:56:59 +0000

    feat: :sparkles: Turn hero element into Carousel
    - Add splidejs (Carousel library) as a dependency
    - Add more images for the Carosel
    - Refactor SCSS colors into a map for easier iteration

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import 'normalize.css';
+import '@splidejs/splide/css';
 import './styles/styles.scss';
+
+import Splide from '@splidejs/splide';
+
+new Splide('.splide', {
+    type: 'loop',
+    arrows: false,
+    autoplay: true,
+}).mount();
